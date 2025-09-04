2a9361414e1db69678c4872f1ec82ef324caa105
nfurcolo <45686704+nfurcolo@users.noreply.github.com>
2025-09-04 17:25:15 +0200

    Add files via upload

---
+++ PowerTradePosition/Enums/PeriodEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerTradePosition.Enums
+{
+    public enum PeriodEnum
+    {
+        MaxSize = 24
+    }
+}
