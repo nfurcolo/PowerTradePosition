d3c2884417f9efbb9be96f97693a26bf9ebb5ba3
nfurcolo <45686704+nfurcolo@users.noreply.github.com>
2025-09-06 15:57:37 +0200

    Add files via upload

---
+++ PowerTradePosition/Logs/WriteLog.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerTradePosition.Logs
+{
+    internal class WriteLogFile
+    {
+        public static bool WriteLog(string strFileName, string strMessage)
+        {
+            try
+            {
+                string docPath =
+                    Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+                FileStream objFilestream = new FileStream(string.Format("{0}\\{1}", docPath, strFileName), FileMode.Append, FileAccess.Write);
+                StreamWriter objStreamWriter = new StreamWriter((Stream)objFilestream);
+                objStreamWriter.WriteLine(strMessage);
+                objStreamWriter.Close();
+                objFilestream.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
